c570a5f7a6898f11ea9fbcaedb7d1c1b940214a3
Abedali <dataentrymax9@gmail.com>
2023-08-29 18:54:22 +0600

    thirs update

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtask/',views.AddTask,name='addtask')
+    path('addtask/',views.AddTask,name='addtask'),
+    path('mark-as-done/<int:nk>',views.MarkAsDone,name='markasdone'),
+    path('mark-as-undo/<int:pk>',views.MarkAsUndo,name='markasundo'),
+    path('mark-edit/<int:pk>',views.MarkEdit,name='edit')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
@@ -7,3 +7,25 @@ def AddTask(request):
     task_field = request.POST.get('taskadd')
     Task.objects.create(task=task_field)
     return redirect('home')
+def MarkAsDone(request,nk):
+    task_done = get_object_or_404(Task,pk=nk)
+    task_done.is_completed = True
+    task_done.save()
+    return redirect('home')
+def MarkAsUndo(request,pk):
+    u_task = get_object_or_404(Task,pk=pk)
+    u_task.is_completed = False
+    u_task.save()
+    return redirect('home')
+def MarkEdit(request,pk):
+    task_edit = get_object_or_404(Task,pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST.get('taskedit')
+        task_edit.task = new_task
+        task_edit.save()
+        return redirect('home')
+    else:
+        context = {
+            'data' : task_edit
+        }
+    return render(request,'edit.html',context)
