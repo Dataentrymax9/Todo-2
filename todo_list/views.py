3470bf50aec54a56f2ffa09b6aebe080014caa09
Abedali <dataentrymax9@gmail.com>
2023-08-29 17:31:28 +0600

    second update

--- todo_list/views.py
+++ todo_list/views.py
@@ -5,7 +5,9 @@ from todo.models import Task
 
 def Home(request):
     Todo_data = Task.objects.filter(is_completed = False).order_by('-update')
+    c_task = Task.objects.filter(is_completed = True).order_by('-update')
     context = {
-        'data1' : Todo_data
+        'data1' : Todo_data,
+        'data2' : c_task
     }
     return render(request,'index.html',context)
